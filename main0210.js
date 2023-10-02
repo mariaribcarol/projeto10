f0b26884f25c069cfebc9d20ad5f5c014e4c0ae4
Maria <mariacarolina789@hotmail.com>
2023-10-02 20:28:03 -0300

    .

---
+++ main0210.js
@@ -0,0 +1,26 @@
+console.log('passei aqui');
+
+function relogio(tempoAlarme) {
+    return new Promise((result, reject) => {
+        let segundos = 0
+        let num = (Math.random() * 100) % 9
+        console.log('num',  num);
+        setInterval(() => {
+            segundos++
+            if (segundos >= num) {
+                reject('OPA! DEU PANE!!!!!!!')
+            }
+            if (segundos == tempoAlarme) {
+                result('BEEP BEEP BEEP')
+            }
+        }, 1000)
+    })
+}
+
+let p = relogio(5);
+console.log('p', p);
+p.then((res) => {
+    console.log('res', res);
+}).catch((resp) => {
+    console.log('resp', resp);
+})
